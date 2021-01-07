c4d5e4793a7f3739fb5a3d0d9650393f59157290
Norlan Tibanear <ntibanear@gmail.com>
2021-01-06 21:34:16 -0500

    Creating Photo Picker

---
+++ QuickGifs/QuickGifs/CustomCameraVC.swift
@@ -0,0 +1,21 @@
+//
+//  CustomCameraVC.swift
+//  QuickGifs
+//
+//  Created by Norlan Tibanear on 1/6/21.
+//
+
+import UIKit
+
+class CustomCameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}//
---
+++ QuickGifs/QuickGifs/PhotoLibraryVC.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoLibraryVC.swift
+//  QuickGifs
+//
+//  Created by Norlan Tibanear on 1/6/21.
+//
+
+import UIKit
+
+class PhotoLibraryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}//
