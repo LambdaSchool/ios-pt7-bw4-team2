1b9e9f0325666c13543bbfa99856f6067303f237
Sammy Alvarado <isammyalvarado@gmail.com>
2021-01-06 21:57:31 -0500

    added collection view

--- QuickGifs/QuickGifs/MyGifsViewController.swift
+++ QuickGifs/QuickGifs/MyGifsViewController.swift
@@ -7,18 +7,30 @@
 
 import UIKit
 
+struct CustomData {
+    var image: UIImage
+}
+
 class MyGifsViewController: UIViewController {
     
+    var image: UIImage?
+    
+    let testData = [
+        CustomData(image: #imageLiteral(resourceName: "john-ko-K7fUYVvmLxk-unsplash")),
+        CustomData(image: #imageLiteral(resourceName: "field-inside-the-emblem-with-cow-mountain-and-plant-892ld")),
+        CustomData(image: #imageLiteral(resourceName: "green-tree-plant-bio-organic-seed-logo-vector-17102220")),
+        CustomData(image: #imageLiteral(resourceName: "leaf-plant-logo-ecology-people-wellness-green-leaves-nature-symbol-icon-set-designs-health-logo-template-business-leaf-107966551")),
+        CustomData(image: #imageLiteral(resourceName: "john-ko-K7fUYVvmLxk-unsplash"))
+    ]
+        
     // creating the colletion Think of me draging out the collection to the storyboard
     private let collectionView: UICollectionView = {
        let layout = UICollectionViewFlowLayout()
-//        layout.minimumInteritemSpacing = 2
         layout.scrollDirection = .vertical
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         cv.register(MyGifsCollectionViewCell.self, forCellWithReuseIdentifier: MyGifsCollectionViewCell.identifier)
         
         cv.contentInset = UIEdgeInsets(top: 20, left: 15, bottom: 0, right: 15)
-        
         
         return cv
     }()
@@ -37,15 +49,34 @@ class MyGifsViewController: UIViewController {
 
     
     // MARK: - Navigation
-/*
-     In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     guard let selectedPath = collectionView else { return }
-     if let target = segue.destination as? UserViewController {
-     target.selectedUser = selectedPath.row
-     }
-     }
-*/
+//    func performSegue(withIdentifier: MyGifsDetialSegue, sender: nil) {
+//
+//    }
+    
+//     In a storyboard-based application, you will often want to do a little preparation before navigation
+//     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//     guard let selectedPath = collectionView else { return }
+//     if let target = segue.destination as? UserViewController {
+//     target.selectedUser = selectedPath.row
+//     }
+//     }
+   
+//     override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+//             if segue!.identifier == "Details" {
+//                 let viewController:ViewController = segue!.destinationViewController as ViewController
+//                 let indexPath = self.tableView.indexPathForSelectedRow()
+//                 viewController.pinCode = self.exams[indexPath.row]
+//
+//             }
+//
+//         }
+  
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let detailVC = segue.destination as! MyGifsDetialViewController
+//        detailVC.photo = self.im
+//
+//        detailVC.photo?.images
+//    }
     
 
 }
@@ -53,12 +84,12 @@ class MyGifsViewController: UIViewController {
 extension MyGifsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     // calling the collection view view collectionView controller or extention.
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return testData.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let gifCell = collectionView.dequeueReusableCell(withReuseIdentifier: MyGifsCollectionViewCell.identifier, for: indexPath) as! MyGifsCollectionViewCell
-        
+        gifCell.data = self.testData[indexPath.row]
         return gifCell
     }
     
@@ -87,9 +118,24 @@ extension MyGifsViewController: UICollectionViewDelegate, UICollectionViewDataSo
         return 10
         // Vertcal spacing between cells
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "MyGifsDetialSegue", sender: nil)
+        let photo = testData[indexPath.item].image
+        self.image = photo
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let detialVC = storyboard.instantiateViewController(identifier: "MyGifsDetialViewController") as! MyGifsDetialViewController
+//        let tabNC = storyboard.instantiateViewController(identifier: "gifTabNC") as! UINavigationController
+
+        
+        detialVC.data = CustomData.init(image: self.image ?? UIImage())
+//        navigationController?.pushViewController(detialVC, animated: true)
+        present(detialVC, animated: true, completion: nil)
+
+        
+        
+//        performSegue(withIdentifier: "MyGifsDetialSegue", sender: self)
+        
+    
     }
     
 }
